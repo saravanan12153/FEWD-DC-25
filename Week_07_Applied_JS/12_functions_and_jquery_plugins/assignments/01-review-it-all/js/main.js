eec38faaa6a382cae56555646acb0c68e127cd6c
rhoesing <rdhoesing@gmail.com>
2016-08-03 18:34:01 -0400

    update

---
+++ Week_07_Applied_JS/12_functions_and_jquery_plugins/assignments/01-review-it-all/js/main.js
@@ -0,0 +1,22 @@
+<<<<<<< Updated upstream
+=======
+$('#orange-button').click(function() {
+  $('#orange-paragraph').css({
+      "background-color": "orange",
+      "color": "white"
+    });
+});
+
+
+
+/*$('#take-me-home), #take-me-home-2, #take-me-home-3').click(function(){
+  $.scrollTo($('html'), {
+    duration: 600;
+  });
+
+
+  return false;
+  console.log("Is this working?")
+});
+*/
+>>>>>>> Stashed changes
