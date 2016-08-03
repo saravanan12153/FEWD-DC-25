8d71bbff8273c780056200182d03492687f19ba9
kevinmcdermott <kevinmcdermott@Kevins-MacBook-Pro.local>
2016-08-03 18:16:23 -0400

    1

--- Week_07_Applied_JS/12_functions_and_jquery_plugins/assignments/01-review-it-all/js/main.js
+++ Week_07_Applied_JS/12_functions_and_jquery_plugins/assignments/01-review-it-all/js/main.js
@@ -4,3 +4,24 @@ $('#take-me-home, #take-me-home-2, #take-me-home-3').click(function(){
   });
   return false;
 });
+
+
+
+
+function FunctionName() {
+$("#orange-paragraph").css({
+	"background-color": "orange",
+	"color": "white"
+});
+}
+$(".button-style").click(FunctionName);
+
+
+
+$("#take-me-home-2").click(function() {
+	var variableStuff = $(".nav-container").html();
+
+alert(variableStuff); 
+
+});
+
