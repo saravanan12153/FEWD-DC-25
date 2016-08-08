2587798e950e566db0b2fecd8ea1a93b3045f435
rhoesing <rdhoesing@gmail.com>
2016-08-08 18:33:14 -0400

    changes

--- Week_07_Applied_JS/13_forms/assignments/01-plug-in-to-it/js/main.js
+++ Week_07_Applied_JS/13_forms/assignments/01-plug-in-to-it/js/main.js
@@ -1,1 +1,20 @@
+$("#take-me-home, #take-me-home-2, #take-me-home-3").click(function() {
+  $.scrollTo('#start-of-page', 800);
+});
 
+$(".yellow").bacon({
+        'type' : 'bezier',
+        'c1' : { x : 10,        y : 0  },
+        'c2' : { x : -115,      y : 170 },
+        'c3' : { x : 35,        y : 340 },
+        'c4' : { x : 15,        y : 480 },
+        'align'  : 'right'
+    }
+);
+
+$(".yellow").bacon({
+        'type' : 'line',
+        'step'      : 5,
+        'align'     : 'right'
+    }
+);
