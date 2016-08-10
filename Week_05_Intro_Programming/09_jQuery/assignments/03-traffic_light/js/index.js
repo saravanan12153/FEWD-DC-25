411a6b0d091ab9fd04a80532e0a9cc43e7b2f6b9
sydneyalexandria <sydneyb@Sydneys-MacBook-Pro.local>
2016-08-10 18:56:46 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 8da38a71f83a62d7cecdb0ef8615236ddd99c57f, reversing
    changes made to 91f87202c7431ac9e5275b0a1b1ed67fc54210c4.

--- Week_05_Intro_Programming/09_jQuery/assignments/03-traffic_light/js/index.js
+++ Week_05_Intro_Programming/09_jQuery/assignments/03-traffic_light/js/index.js
@@ -1,8 +1,1 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
-
-function redLight(){
-$('stopButton').click(function(){
-	$('stopLight).toggleClass ('red');
-
-});
-
--- Week_06_Practical_JS/11_conditionals_this/assignments/02-comparison-time/js/main.js
+++ Week_06_Practical_JS/11_conditionals_this/assignments/02-comparison-time/js/main.js
@@ -51,18 +51,13 @@ if (daysInAWeek <= blackJack) {
 }
 
 // 4- Another integer comparison, using else if
-<<<<<<< Updated upstream
 
 daysInAWeek = 27;
-=======
-var blackJack = 21, daysInAWeek = 27;
->>>>>>> Stashed changes
 if (blackJack <= daysInAWeek) {
   $("#blackjack-container").html("<img src=\"http://www.clipartbest.com/cliparts/Kin/oyR/KinoyRpiq.jpeg\">");
   $("#blackjack-container img").css({
     "height": "300px",
     "width": "300px"
-<<<<<<< Updated upstream
   });
 } else if (blackJack >= daysInAWeek) {
   $("#blackjack-container").html("<img src=\"http://blackjacklife.com/wp-content/uploads/2012/01/blackjack-picture-1.jpg\">");
@@ -102,34 +97,6 @@ if (octopusLegs !== maxVolumeLevel) {
 }
 console.log(checkVal);
 
-=======
-
-  });
-    // false, blackJack is not less than or equal to daysInAWeek
-  } else if (blackJack >= daysInAWeek) {
-    $("#blackjack-container").html("<img src=\"http://blackjacklife.com/wp-content/uploads/2012/01/blackjack-picture-1.jpg\">");
-  }
-  
-// 5- Setting a Boolean based on variable values
-// Note the program flow here, this is getting more advanced
-var octopusLegs = 8, maxVolumeLevel = 45;
-var checkVal;
-
-
-if (octopusLegs !== maxVolumeLevel) {
-  var checkVal = false;
-} else if (octopusLegs === maxVolumeLevel) {
-  checkVal = true;
-
-}
-
-console.log(checkVal);
-
-
-
-
-
->>>>>>> Stashed changes
 function calamari () {
   if (checkVal === true) {
     $(".box-example-1").css({
@@ -164,10 +131,5 @@ var sum;
 function addThings(val1, val2) {
   sum = val1 + val2;
 }
-<<<<<<< Updated upstream
 addThings(1414, 765);
 console.log(sum);
-=======
-
-addThings(1414, null);
->>>>>>> Stashed changes
